bf44e4ea809d0f804e0be72cb8ef8f9586dd036f
Howard <tgfc041@tgfc.com>
2019-08-26 16:25:01 +0800

    employee 建立

---
+++ src/main/java/com/tgfc/library/entity/employee.java
@@ -0,0 +1,63 @@
+package com.tgfc.library.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "employee")
+public class employee implements Serializable {
+
+    @Id
+    private int id;
+    @Column(name="account")
+    private String account;
+    @Column(name="name")
+    private String name;
+    @Column(name="email")
+    private String email;
+    @Column(name="password")
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
