5908469d4a414e3f8bb06255eb94b577f07b074c
TGFC062 <tgfc062@tgfc.tw>
2019-08-27 13:44:04 +0800

    新增IRecordsRepository

---
+++ src/main/java/com/tgfc/library/repository/IRecordsRepository.java
@@ -0,0 +1,22 @@
+package com.tgfc.library.repository;
+
+import com.tgfc.library.entity.Records;
+import com.tgfc.library.entity.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+@Repository
+public interface IRecordsRepository extends JpaRepository<Records,Integer> {
+
+    @Override
+    Records getOne(Integer integer);
+    @Override
+    Page<Records> findAll(Pageable pageable);
+    @Query("SELECT r from Records r where r.borrowDate<=?1 AND r.returnDate>=?2")
+    Page<Reservation> findByTimeInterval(Date borrowDate, Date returnDate, Pageable pageable);
+}
