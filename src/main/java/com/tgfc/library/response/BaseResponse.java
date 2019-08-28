22612024834f50c6c28b94ad3b8362f4d4ba924a
TGFC056 <tgfc056@tgfc.tw>
2019-08-28 09:46:24 +0800

    新增基礎回傳物件

---
+++ src/main/java/com/tgfc/library/response/BaseResponse.java
@@ -0,0 +1,24 @@
+package com.tgfc.library.response;
+
+public class BaseResponse {
+
+    private String message;
+
+    private boolean status;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
