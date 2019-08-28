d5ccee5cb7cc685b1dcc4df511f2323b6bf88bf3
TGFC062 <tgfc062@tgfc.tw>
2019-08-28 15:17:06 +0800

    更新PageableRequest

---
+++ src/main/java/com/tgfc/library/com/tgfc/library/util/ContextUtil.java
@@ -0,0 +1,15 @@
+package com.tgfc.library.com.tgfc.library.util;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class ContextUtil {
+
+    public static SecurityContext getContext(){
+        return SecurityContextHolder.getContext();
+    }
+
+    public static Object getPrincipal(){
+        return  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
