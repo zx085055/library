e666af6d250b211e72dd0d9cff8722c4d542305a
Howard <tgfc041@tgfc.com>
2019-08-29 12:16:56 +0800

    修改登入bug

--- src/main/java/com/tgfc/library/controller/TestController.java
+++ src/main/java/com/tgfc/library/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.tgfc.library.controller;
 
-import com.tgfc.library.service.imp.SendMailService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class TestController {
-    @Autowired
-    private SendMailService sendMailService;
 
-    @GetMapping(value = "/{id}")
+
+    @GetMapping(value = "/api/{id}")
     public String get(){
 //        sendMailService.sendMail("我是標題","你好","tgfc061@tgfc.tw");
         return "Hello Security";
