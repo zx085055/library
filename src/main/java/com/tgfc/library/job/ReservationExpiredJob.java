7d1fef5d77712f8c9234f13f7800cfa63dd2ef57
Bobson <tgfc063@tgfc.tw>
2019-08-29 14:46:01 +0800

    排程showdoc

--- src/main/java/com/tgfc/library/job/ReservationExpiredJob.java
+++ src/main/java/com/tgfc/library/job/ReservationExpiredJob.java
@@ -5,9 +5,13 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 /**
- * TODO 預約到期
+ * TODO 預約到期通知
  */
 public class ReservationExpiredJob implements Job {
+    /**
+     * 傳入值:scheduleMailListModel (收件人，收件人信箱，書名，到期日期)
+     * 邏輯:通過model組成文本，呼叫ScheduleService的批量寄信通知
+     */
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
---
+++ src/main/java/com/tgfc/library/service/imp/ScheduleService.java
@@ -0,0 +1,6 @@
+package com.tgfc.library.service.imp;
+
+import com.tgfc.library.service.IScheduleService;
+
+public class ScheduleService implements IScheduleService {
+}
