bd99a2319eb53811b8cb3b6aa9c2b4214fb866a3
TGFC061 <tgfc061@tgfc.tw>
2019-09-02 13:46:31 +0800

    刪除不必要代碼

--- src/main/java/com/tgfc/library/util/ContextUtil.java
+++ src/main/java/com/tgfc/library/util/ContextUtil.java
@@ -1,7 +1,6 @@
 package com.tgfc.library.util;
 
 import com.tgfc.library.response.EmployeeResponse;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -14,14 +13,6 @@ public class ContextUtil {
 
     public static Object getPrincipal(){
         return  SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
-
-    public static Authentication getAuthentication() {//TODO 測試用
-        return SecurityContextHolder.getContext().getAuthentication();
-    }
-
-    public static void setAuthentication(Authentication authentication) {
-        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     public static String getAccount (){
