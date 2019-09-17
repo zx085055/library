1cc0c27a2a34a802d4581cb12dfb39aefa4f9e8f
TGFC061 <tgfc061@tgfc.tw>
2019-09-17 17:59:46 +0800

    LogAspect 獲取RequestParam 修正

--- src/main/java/com/tgfc/library/aop/LogAspect.java
+++ src/main/java/com/tgfc/library/aop/LogAspect.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 @Aspect
 @Component
@@ -30,27 +31,33 @@ public class LogAspect {
         String className = null;
         Object[] args = joinPoint.getArgs();
         for (Object info : args) {
-            className = info.getClass().getName();// 獲取對象類型
-            className = className.substring(className.lastIndexOf(".") + 1);
-            rs.append("[參數").append(Arrays.toString(args).indexOf(info.toString())).append("，類型：").append(className).append("，值：");
-            Method[] methods = info.getClass().getDeclaredMethods();// 獲取對象的所有方法
-            for (Method method : methods) {// 遍歷方法，判斷get方法
-                Parameter[] parameters = method.getParameters();
-                if (parameters.length > 0) {
-                    for (Method methodGet : methods) {// 遍歷方法，判斷get方法
-                        String methodGetName = methodGet.getName().toUpperCase();
-                        if (methodGetName.contains("GET") && methodGetName.contains(parameters[0].getName().toUpperCase())) {// 判斷是不是get方法，是get方法
-                            Object rsValue = null;
-                            rsValue = methodGet.invoke(info);// 調用get方法，獲取返回值
-                            if (rsValue == null) {// 沒有返回值
-                                continue;
+            Pattern pattern = Pattern.compile("[0-9]*");
+            if (pattern.matcher(info.toString()).matches()) {
+                rs.append("[參數").append(Arrays.toString(args).indexOf(info.toString())).append("，類型：").append(className).append("，值：");
+                rs.append("(").append("id").append(" : ").append(info).append(")");// 將值加入內容中
+            } else {
+                className = info.getClass().getName();// 獲取對象類型
+                className = className.substring(className.lastIndexOf(".") + 1);
+                rs.append("[參數").append(Arrays.toString(args).indexOf(info.toString())).append("，類型：").append(className).append("，值：");
+                Method[] methods = info.getClass().getDeclaredMethods();// 獲取對象的所有方法
+                for (Method method : methods) {// 遍歷方法，判斷get方法
+                    Parameter[] parameters = method.getParameters();
+                    if (parameters.length > 0) {
+                        for (Method methodGet : methods) {// 遍歷方法，判斷get方法
+                            String methodGetName = methodGet.getName().toUpperCase();
+                            if (methodGetName.contains("GET") && methodGetName.contains(parameters[0].getName().toUpperCase())) {// 判斷是不是get方法，是get方法
+                                Object rsValue = null;
+                                rsValue = methodGet.invoke(info);// 調用get方法，獲取返回值
+                                if (rsValue == null) {// 沒有返回值
+                                    continue;
+                                }
+                                rs.append("(").append(parameters[0].getName()).append(" : ").append(rsValue).append(")");// 將值加入內容中
                             }
-                            rs.append("(").append(parameters[0].getName()).append(" : ").append(rsValue).append(")");// 將值加入內容中
                         }
                     }
                 }
+                rs.append("]");
             }
-            rs.append("]");
         }
         logger.info("START_ARGS : " + rs.toString());
     }
