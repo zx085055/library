ef280152fe80d25725771d047800b20e1bf82149
TGFC061 <tgfc061@tgfc.tw>
2019-10-01 15:48:40 +0800

    出借紀錄複合查詢修正

--- src/main/java/com/tgfc/library/service/imp/RecordsService.java
+++ src/main/java/com/tgfc/library/service/imp/RecordsService.java
@@ -51,7 +51,6 @@ public class RecordsService implements IRecordsService {
             baseResponse.setMessage("查詢成功");
             baseResponse.setStatus(true);
         } else {
-            keyword = (keyword.isEmpty()) ? null : keyword;
             Page<Records> records = recordsRepository.getRecordsByNameLikeAndStatus(keyword, status, pageable);
             Map<String,Object> data = new HashMap<>();
             data.put("totalCount",records.getTotalElements());
