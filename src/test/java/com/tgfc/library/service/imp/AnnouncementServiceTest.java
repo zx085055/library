8fe54f787deb1467ff4f632280e58a37d7a5b408
TGFC061 <tgfc061@tgfc.tw>
2019-10-08 16:54:22 +0800

    公告狀態切換單元測試

---
+++ src/test/java/com/tgfc/library/service/imp/AnnouncementServiceTest.java
@@ -0,0 +1,40 @@
+package com.tgfc.library.service.imp;
+
+import com.tgfc.library.LibraryApplication;
+import com.tgfc.library.entity.Announcement;
+import com.tgfc.library.entity.EmployeeSafty;
+import com.tgfc.library.repository.IAnnouncementRepository;
+import com.tgfc.library.repository.IEmployeeRepositorySafty;
+import com.tgfc.library.response.BaseResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = LibraryApplication.class)
+@Transactional
+class AnnouncementServiceTest {
+
+    @Autowired
+    IAnnouncementRepository announcementRepository;
+
+    @Autowired
+    IEmployeeRepositorySafty employeeRepository;
+
+    @Test
+    void changeStatus() {
+        BaseResponse baseResponse = new BaseResponse();
+
+        EmployeeSafty employee = employeeRepository.findById("TGFC061").get();
+        Announcement existAnnouncement = announcementRepository.findById(7).get();
+        existAnnouncement.setEmployee(employee);
+        existAnnouncement.setStatus(true);
+        announcementRepository.save(existAnnouncement);
+        baseResponse.setStatus(true);
+        baseResponse.setMessage("切換成功");
+        System.out.println(baseResponse);
+    }
+}
