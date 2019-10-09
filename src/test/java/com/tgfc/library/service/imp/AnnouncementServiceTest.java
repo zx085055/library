f1f4965f796ab7db17583a78c1587d769b3dc6ea
TGFC061 <tgfc061@tgfc.tw>
2019-10-09 09:47:17 +0800

    公告狀態切換單元測試

--- src/test/java/com/tgfc/library/service/imp/AnnouncementServiceTest.java
+++ src/test/java/com/tgfc/library/service/imp/AnnouncementServiceTest.java
@@ -7,13 +7,10 @@ import com.tgfc.library.repository.IAnnouncementRepository;
 import com.tgfc.library.repository.IEmployeeRepositorySafty;
 import com.tgfc.library.response.BaseResponse;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = LibraryApplication.class)
 @Transactional
 class AnnouncementServiceTest {
