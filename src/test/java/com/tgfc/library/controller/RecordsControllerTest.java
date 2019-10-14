a188d07f9d611737fecaa601d3e65debf97eae9a
TGFC062 <tgfc062@tgfc.tw>
2019-10-14 16:15:55 +0800

    紀錄測試完成

--- src/test/java/com/tgfc/library/controller/RecordsControllerTest.java
+++ src/test/java/com/tgfc/library/controller/RecordsControllerTest.java
@@ -1,7 +1,13 @@
 package com.tgfc.library.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.tgfc.library.LibraryApplication;
+import com.tgfc.library.request.PageableRequest;
+import com.tgfc.library.request.RecommendPageRequest;
+import com.tgfc.library.request.RecordsPageRequest;
+import com.tgfc.library.request.RecordsSearchPageRequest;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -19,6 +26,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -114,6 +122,51 @@ class RecordsControllerTest {
         MockHttpServletResponse response = mockMvc.perform(requestBuilder).andReturn().getResponse();
         Assertions.assertEquals(HttpStatus.OK.value(),response.getStatus());
         Assertions.assertEquals("查詢成功",new JSONObject(response.getContentAsString()).get("message"));
+    }
+
+    @Test
+    public void testFindByDate() throws Exception{
+        RecordsSearchPageRequest records = new RecordsSearchPageRequest();
+        records.setStartDate(new Date(2019,9,3));
+        records.setEndDate(new Date(2019,9,10));
+
+        Gson gson = new GsonBuilder().setDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz").create();
+        String jsonData = gson.toJson(records);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/records/findByDate").contentType(MediaType.APPLICATION_JSON).session(session).content(jsonData);
+        MockHttpServletResponse response = mockMvc.perform(requestBuilder).andReturn().getResponse();
+        Assertions.assertEquals(HttpStatus.OK.value(),response.getStatus());
+        Assertions.assertEquals("查詢成功",new JSONObject(response.getContentAsString()).get("message"));
+
+    }
+
+    @Test
+    public void testFindByEmpId() throws Exception{
+        PageableRequest request = new PageableRequest();
+
+        Gson gson = new GsonBuilder().create();
+        String jsonData = gson.toJson(request);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/records/findByEmpId").contentType(MediaType.APPLICATION_JSON).session(session).content(jsonData);
+        MockHttpServletResponse response = mockMvc.perform(requestBuilder).andReturn().getResponse();
+        Assertions.assertEquals(HttpStatus.OK.value(),response.getStatus());
+        Assertions.assertEquals("查詢成功",new JSONObject(response.getContentAsString()).get("message"));
+    }
+
+    @Test
+    public void testFindByDateWithEmpId() throws Exception{
+        RecordsSearchPageRequest request = new RecordsSearchPageRequest();
+        request.setStartDate(new Date(2019,9,3));
+        request.setEndDate(new Date(2019,9,10));
+
+        Gson gson = new GsonBuilder().setDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz").create();
+        String jsonData = gson.toJson(request);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/records/findByDateWithEmpId").contentType(MediaType.APPLICATION_JSON).session(session).content(jsonData);
+        MockHttpServletResponse response = mockMvc.perform(requestBuilder).andReturn().getResponse();
+        Assertions.assertEquals(HttpStatus.OK.value(),response.getStatus());
+        Assertions.assertEquals("查詢成功",new JSONObject(response.getContentAsString()).get("message"));
+
     }
 
 }
