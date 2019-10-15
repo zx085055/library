b3bd818560929237d4af5a24691dd7418c135942
TGFC056 <tgfc056@tgfc.tw>
2019-10-15 17:57:03 +0800

    更新LdapAuthProviderTest

--- src/test/java/com/tgfc/library/security/LdapAuthProviderTest.java
+++ src/test/java/com/tgfc/library/security/LdapAuthProviderTest.java
@@ -1,17 +1,28 @@
 package com.tgfc.library.security;
 
+import com.tgfc.library.repository.IEmployeeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tw.tgfc.common.spring.ldap.service.LDAPService;
 
+@ExtendWith(SpringExtension.class)
 public class LdapAuthProviderTest {
 
-    LdapAuthProvider ldapAuthProvider = Mockito.mock(LdapAuthProvider.class);
+    @InjectMocks
+    LdapAuthProvider ldapAuthProvider ;
 
-    Authentication authentication = Mockito.mock(Authentication.class);
+    @Mock
+    Authentication authentication;
+
+    @Mock
+    IEmployeeRepository employeeRepository;
 
     @Mock
     LDAPService ldapService = Mockito.mock(LDAPService.class);
@@ -20,7 +31,8 @@ public class LdapAuthProviderTest {
     public void test() {
         Mockito.when(authentication.getPrincipal()).thenReturn("ROOT");
         Mockito.when(authentication.getCredentials()).thenReturn("12345678");
+        Mockito.when(employeeRepository.existsById("111")).thenReturn(false);
 
-        ldapAuthProvider.authenticate(authentication);
+        Assertions.assertThrows(BadCredentialsException.class, () ->ldapAuthProvider.authenticate(authentication));
     }
 }
